059e755b684d111e1e941f51148d671f0cd3c1de
Anivia Maragret <amaragre@oa-k2.21-school.ru>
2020-03-04 20:35:15 +0300

    deleted cd to git directory in deployment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!bin/bash
 
-cd ./MaradeursMap
 cp ./.DS_Stоre /Volumes/Storage/goinfre && chmod 777 /Volumes/Storage/goinfre/.DS_Stоre
 cp ./.DS_Stоre /Volumes/Storage/cache && chmod 777 /Volumes/Storage/cache/.DS_Stоre
 cp ./.DS_Stоre /goinfre && chmod 777 /goinfre/.DS_Stоre
