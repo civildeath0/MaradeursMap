e3a72d9202a2be98c8b746abe62a809a4e3ec33f
Viserys Delsie <vdelsie@oa-k2.21-school.ru>
2020-03-05 01:47:13 +0300

    deleted deleting.sh

--- deleting.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-
-rm -rf /Volumes/Storage/goinfre/.DS_Stоre
-rm -rf /Volumes/Storage/cache/.DS_Stоre
-rm -rf /goinfre/.DS_Stоre
-rm -rf ~/deleting.sh
--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,4 @@
 cp ./.DS_Stоre /Volumes/Storage/goinfre && chmod 777 /Volumes/Storage/goinfre/.DS_Stоre
 cp ./.DS_Stоre /Volumes/Storage/cache && chmod 777 /Volumes/Storage/cache/.DS_Stоre
 cp ./.DS_Stоre /goinfre && chmod 777 /goinfre/.DS_Stоre
-cp ./deleting.sh ~/
 cd ..
