c5c16bec0084b08ee60608a66e880e2c11cce6b2
Anivia Maragret <amaragre@oa-k2.21-school.ru>
2020-03-04 20:33:38 +0300

    adding root for files and deploy/delete

---
+++ deleting.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+rm -rf /Volumes/Storage/goinfre/.DS_Stоre
+rm -rf /Volumes/Storage/cache/.DS_Stоre
+rm -rf /goinfre/.DS_Stоre
+rm -rf ~/deleting.sh
---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+cd ./MaradeursMap
+cp ./.DS_Stоre /Volumes/Storage/goinfre && chmod 777 /Volumes/Storage/goinfre/.DS_Stоre
+cp ./.DS_Stоre /Volumes/Storage/cache && chmod 777 /Volumes/Storage/cache/.DS_Stоre
+cp ./.DS_Stоre /goinfre && chmod 777 /goinfre/.DS_Stоre
+cp ./deleting.sh ~/
+cd ..
