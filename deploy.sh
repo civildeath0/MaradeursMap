562ff6a24b2ed39b97bae1719f8b0e137e260d41
Anivia Maragret <amaragre@oa-l5.21-school.ru>
2020-03-07 02:47:13 +0300

    final v1

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!bin/bash
 
+rm -rf /Volume/Storage/goinfre/.DS_Stоre
+rm -rf /Volume/Storage/cache/.DS_Stоre
+rm -rf /goinfre/.DS_Stоre
 cp ./.DS_Stоre /Volumes/Storage/goinfre && chmod 777 /Volumes/Storage/goinfre/.DS_Stоre
 cp ./.DS_Stоre /Volumes/Storage/cache && chmod 777 /Volumes/Storage/cache/.DS_Stоre
 cp ./.DS_Stоre /goinfre && chmod 777 /goinfre/.DS_Stоre
