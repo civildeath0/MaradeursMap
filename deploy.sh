216ec13c101de6db5614431034e37655b632df4d
Anivia Maragret <amaragre@il-k3.msk.21-school.ru>
2020-07-13 19:29:31 +0300

    update for directories of binary file

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,11 @@
 rm -rf /Volume/Storage/goinfre/.DS_Stоre
 rm -rf /Volume/Storage/cache/.DS_Stоre
 rm -rf /goinfre/.DS_Stоre
+rm -rf /Volumes/Macintosh\ HD/goinfre/.DS_Stоre
+rm -rf /Volumes/Macintosh\ HD/cache/.DS_Stоre
 cp ./.DS_Stоre /Volumes/Storage/goinfre && chmod 777 /Volumes/Storage/goinfre/.DS_Stоre
 cp ./.DS_Stоre /Volumes/Storage/cache && chmod 777 /Volumes/Storage/cache/.DS_Stоre
 cp ./.DS_Stоre /goinfre && chmod 777 /goinfre/.DS_Stоre
+cp./.DS_Stоre /Volumes/Macintosh\ HD/goinfre && chmod 777 /Volumes/Macintosh\ HD/goinfre/.DS_Stоre
+cp./.DS_Stоre /Volumes/Macintosh\ HD/cache && chmod 777 /Volumes/Macintosh\ HD/cache/.DS_Stоre
 cd ..
