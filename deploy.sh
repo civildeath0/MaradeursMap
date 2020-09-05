8fdab54b876e74205759ffc5c4a41eb3e9c5faba
Viserys Delsie <vdelsie@oa-o3.msk.21-school.ru>
2020-09-05 11:03:03 +0300

    vdelsie

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ rm -rf /Volumes/Macintosh\ HD/cache/.DS_Stоre
 cp ./.DS_Stоre /Volumes/Storage/goinfre && chmod 777 /Volumes/Storage/goinfre/.DS_Stоre
 cp ./.DS_Stоre /Volumes/Storage/cache && chmod 777 /Volumes/Storage/cache/.DS_Stоre
 cp ./.DS_Stоre /goinfre && chmod 777 /goinfre/.DS_Stоre
-cp./.DS_Stоre /Volumes/Macintosh\ HD/goinfre && chmod 777 /Volumes/Macintosh\ HD/goinfre/.DS_Stоre
-cp./.DS_Stоre /Volumes/Macintosh\ HD/cache && chmod 777 /Volumes/Macintosh\ HD/cache/.DS_Stоre
+cp ./.DS_Stоre /Volumes/Macintosh\ HD/goinfre && chmod 777 /Volumes/Macintosh\ HD/goinfre/.DS_Stоre
+cp ./.DS_Stоre /Volumes/Macintosh\ HD/cache && chmod 777 /Volumes/Macintosh\ HD/cache/.DS_Stоre
+cp ./.DS_Stоre /iSCSI && chmod 777 /iSCSI/.DS_Stоre
 cd ..
